b515eae553a5de444ad72a441972529d925ed2cc
aryan-saxena-outscal <aryan.saxena@outscal.com>
2024-02-15 18:51:56 +0530

    Refactor openAllCells()

--- Array-Minesweeper/source/Gameplay/Board/BoardController.cpp
+++ Array-Minesweeper/source/Gameplay/Board/BoardController.cpp
@@ -254,11 +254,6 @@ namespace Gameplay
 
 		void BoardController::openAllCells()
 		{
-			if (board_state == BoardState::FIRST_CELL)
-			{
-				populateBoard(sf::Vector2i(0, 0));
-			}
-
 			for (int a = 0; a < number_of_rows; ++a)
 			{
 				for (int b = 0; b < number_of_colums; ++b)
